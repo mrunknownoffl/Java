54f7b50a0a13bb1ffd2a3fc2bdc1ca4502ec72c2
mrunknownoffl <79073074+mrunknownoffl@users.noreply.github.com>
2021-08-06 18:07:44 +0530

    Add files via upload

---
+++ FirstApplet.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.applet.*;
+public class FirstApplet extends Applet
+{
+public void paint(Graphics g)
+{
+g.drawString("this is first applet program",100,100);
+}
+}
+/*<applet code="FirstApplet" width=300 height=300></applet>*/
